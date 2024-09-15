549246988a98af0ee273d5e728dc99a1638776fd
Adriaan Groenenboom <agboom@pm.me>
2024-09-15 17:21:03 +0200

    Rename `osmc` to `osmc_vero`
    - Add platform-specific fixes to `osmc_vero` launcher

--- resources/launchscripts/osmc_vero/launch.sh
+++ resources/launchscripts/osmc_vero/launch.sh
@@ -4,9 +4,14 @@ resume() {
   killall -CONT kodi.bin
 }
 
-trap resume EXIT INT TERM
+trap resume EXIT
 
 killall -STOP kodi.bin
+
+# prevent black screen after playing video
+echo 1 > /sys/class/graphics/fb0/blank
+echo "rm default" > /sys/class/vfm/map
+echo add default decoder ppmgr deinterlace amlvideo amvideo > /sys/class/vfm/map
 
 mv logfile logfile.old
 LD_PRELOAD="/usr/local/lib/libmoonlight-aml.so /usr/osmc/lib/libamavutils.so /usr/osmc/lib/libamadec.so /usr/osmc/lib/libamcodec.so" \
